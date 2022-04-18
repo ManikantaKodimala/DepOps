e6563da138e043c40496216c4a48aef21ac28fd1
ManikantaKodimala <manikanta.kodimala@everest.engineering>
2022-04-18 12:07:25 +0530

    Python server

---
+++ PythonServer/python_server.py
@@ -0,0 +1,29 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+host="10.10.0.24"
+port=8000
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+        self.wfile.write(bytes("<html><head><title>Sample Server - Astro</title></head>", "utf-8"))
+        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
+        self.wfile.write(bytes("<body>", "utf-8"))
+        self.wfile.write(bytes("<p></p>", "utf-8"))
+        self.wfile.write(bytes("<p>Response Headers: %s</p>" % self.headers, "utf-8"))
+        self.wfile.write(bytes("</body></html>", "utf-8"))
+
+    def do_POST(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+
+        message = "Hello, World ! Here is a POST response"
+        self.wfile.write(bytes(message, "utf8"))
+
+if __name__== '__main__':
+    with HTTPServer((host, port), handler) as server:
+        server.serve_forever()
+    
