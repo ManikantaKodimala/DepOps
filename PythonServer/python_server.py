88344e7aa4f9c05eec52b789db201e32ea8f2d7c
Mohammed Faizuddin <47523926+FaizuddinEverest@users.noreply.github.com>
2022-04-22 14:41:28 +0530

    fix: better title for the page

--- PythonServer/python_server.py
+++ PythonServer/python_server.py
@@ -8,7 +8,7 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type','text/html')
         self.end_headers()
-        self.wfile.write(bytes("<html><head><title>Sample Server - Astro</title></head>", "utf-8"))
+        self.wfile.write(bytes("<html><head><title>A whoami server</title></head>", "utf-8"))
         self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
         self.wfile.write(bytes("<body>", "utf-8"))
         self.wfile.write(bytes("<p></p>", "utf-8"))
